3a2033962f5d5c51458ac9f19102480f64dd3629
richardm <richard.mason@eastsussex.gov.uk>
2014-08-08 15:52:51 +0100

    Add model for built-in image document type

---
+++ Image.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Escc.Umbraco
+{
+    public class Image
+    {
+        public Uri ImageUrl { get; set; }
+        public string AlternativeText { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
